95d982029bcf78118c8c4db6bb3dc9c4df5f5694
pop-pi <578410267@qq.com>
2023-09-09 16:18:08 +0800

    C++ Primer第三章

---
+++ C++ Primer 3/3_1.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+// 1.11Á·Ï°
+//int main()
+//{
+//	int a, b;
+//	cin >> a >> b;
+//	int min, max;
+//	if (a <= b)
+//	{
+//		min = a;
+//		max = b;
+//	}
+//	else {
+//		min = b;
+//		max = a;
+//	}
+//	for (int i = a; i <= b; i++) {
+//		cout << i << endl;
+//	}
+//}
